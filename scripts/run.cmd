1c45de3d2ff56e19187b4ba71f91c95e55cefc11
John Arne Føreland <john.foreland@me.com>
2020-03-17 16:31:57 +0100

    Update run.cmd

--- scripts/run.cmd
+++ scripts/run.cmd
@@ -3,8 +3,8 @@
 call cls
 
 cd .ssdx
-call git pull
+call git pull >nul 2>&1
 cd ..
 call .ssdx\venv\Scripts\activate
-call pip install -r .ssdx\python\requirements.txt
+call pip install -r .ssdx\python\requirements.txt >nul 2>&1
 call python .ssdx\python\run.py
