0fbf74e61e0f37ce08ca17df2e7534d01a3aeac4
Eugenijus <eugenijus.margalikas@nav.no>
2022-05-30 13:27:36 +0200

    Revert "create org passing 'co' as argument"
    This reverts commit 3d5545c9860b114b185c01ed1d35c02890bc3fd2.

--- python/run.py
+++ python/run.py
@@ -5,11 +5,6 @@ import subscripts.menu as menu
 
 if __name__ == '__main__':
 	term = menu.init()
-	if len(sys.argv) > 1:
-		param_name = sys.argv[1]
-		if (param_name == "co"):
-			menu.org.createScratchOrg(term)
-			sys.exit()
 	menu.show(term)
 
 
